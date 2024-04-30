58d3305c5ca22ecbaaab1ca61dde5b517b831705
Michael Feleke <felekemichael999@gmail.com>
2024-05-01 00:17:32 +0300

    Refactor startStopwatch function in timer.js

--- timer.js
+++ timer.js
@@ -3,3 +3,21 @@ let pausedTime = 0;
 let running = false;
 let laps = [];
 let lastDisplayedTime = "";
+
+function startStopwatch() {
+  if (!running) {
+    if (pausedTime === 0) {
+      startTime = Date.now();
+    } else {
+      startTime = Date.now() - pausedTime;
+      pausedTime = 0;
+    }
+    running = true;
+    updateStopwatch();
+    document.getElementById("startBtn").innerText = "Pause";
+  } else {
+    running = false;
+    pausedTime = Date.now() - startTime;
+    document.getElementById("startBtn").innerText = "Resume";
+  }
+}
